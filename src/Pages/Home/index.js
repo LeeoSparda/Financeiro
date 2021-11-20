1350810f8d47c8b6fcd7d6ed35f83d0633ee5d2d
Leonardo Borges <leonardo_hitman@hotmail.com>
2021-11-20 00:44:15 -0300

    versao 1.3

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,23 +1,41 @@
-import React, {useContext} from "react";
-import {View, Text, Button} from 'react-native';
-import { useNavigation } from "@react-navigation/core";
+import React, { useContext, useState } from 'react';
+import { View, Text, Button } from 'react-native';
 
-import { AuthContext } from "../../contexts/auth";
+import { AuthContext } from '../../contexts/auth';
+import Header from '../../components/Header';
+import HistoricoList from '../../components/HistoricoList';
 
-export default function Home(){
+import { Background, Container, Nome, Saldo, Title, List} from './styles';
 
-    const {user, signOut} = useContext(AuthContext);
+export default function Home() {
+  const [historico, setHistorico] = useState([
+    {key: '1', tipo: 'receita', valor: 1200},
+    {key: '2', tipo: 'despesa', valor: 200},
+    {key: '3', tipo: 'receita', valor: 40},
+    {key: '4', tipo: 'receita', valor: 89.62},
+    {key: '5', tipo: 'despesa', valor: 500},
+    {key: '6', tipo: 'despesa', valor: 310},
+  ]);
 
-    return(
-        <View>
-            <Text> Tela de home </Text>
-            <Text>{user && user.nome}</Text>
-            <Text>{user && user.email}</Text>
-            <Button
-                title = "Sair da Conta"
-                onPress={ () => signOut()}
-            />
-            
-        </View>
-    );
+  const { user } = useContext(AuthContext);
+
+ return (
+    <Background>
+      <Header/>
+      <Container>
+        <Nome>{user && user.nome}</Nome>
+        <Saldo>R$ 123,00</Saldo>
+      </Container>
+
+      <Title>Ultimas movimentações</Title>
+
+      <List
+      showsVerticalScrollIndicator={false}
+      data={historico}
+      keyExtractor={ item => item.key}
+      renderItem={ ({ item }) => ( <HistoricoList data={item} /> )}
+      />
+
+    </Background>
+  );
 }
--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,14 +1,37 @@
 import React from "react";
-import {createStackNavigator} from '@react-navigation/stack';
-import Home from '../Pages/Home';
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-const AppStack = createStackNavigator();
+import Home from '../Pages/Home';
+import New from '../Pages/New';
+import Profile from "../Pages/Profile";
+
+const AppDrawer = createDrawerNavigator();
 
 function AppRoutes(){
     return(
-        <AppStack.Navigator>
-            <AppStack.Screen name ="Home" component={Home}/>
-        </AppStack.Navigator>
+        <AppDrawer.Navigator
+        drawerStyle={{
+            backgroundColor: '#171717'
+        }}
+        drawerContentOptions={{
+            labelStyle:{
+                fontWeight: 'bold'
+            },
+            activeTintColor: '#FFF',
+            activeBackgroundColor: '#00b94a',
+            inactiveTintColor: '#000',
+            inactiveTintColor: '#DDD',
+            itemStyle:{
+                marginVertical: 5,
+            } 
+        }}
+        >
+            <AppDrawer.Screen name ="Home" component={Home}/>
+            <AppDrawer.Screen name ="Registrar" component={New}/>
+            <AppDrawer.Screen name ="Perfil" component={Profile}/>
+
+
+        </AppDrawer.Navigator>
     )
 }
 
